d2f8f0e20f7b39a9af6dc6fa2b518d1c65f317e7
Andy Li <alii0005@student.monash.edu>
2021-09-11 11:50:34 +1000

    Pages -Links

--- menu.php
+++ menu.php
@@ -1,1 +1,34 @@
-<?php
+<html>
+<head>
+    <title>Resonant With World Menu</title>
+    <link rel="stylesheet" type="text/css" href="styles/style.css" media=”screen” />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+
+<ul class="nav justify-content-center">
+    <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Resonant With World</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link active" href="/index.php">Home</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/product.php">Products</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/client.php">Client</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/photoshoot.php">Photoshoot</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/category.php">Category</a>
+    </li>
+</ul>
+
+</body>
+
+</html>
+
--- product.php
+++ product.php
@@ -1,1 +1,18 @@
-3<?php
+<html>
+<head>
+    <title>Resonant With World Product</title>
+    <link rel="stylesheet" type="text/css" href="styles/style.css" media=”screen” />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+
+<?php include('./menu.php');?>
+
+<h1>Resonant With World</h1>
+
+
+</body>
+
+</html>
+
