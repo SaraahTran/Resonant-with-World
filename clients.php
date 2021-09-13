b75fb6a683fa24129ecff7f8e369975d8be4382c
Andy Li <alii0005@student.monash.edu>
2021-09-13 12:42:20 +1000

    Client -View

--- clients.php
+++ clients.php
@@ -10,7 +10,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;700;800&display=swap" rel="stylesheet">
 </head>
 
+
 <body>
+
+
+</table>
 
 <?php include('./menu.php');?>
 
@@ -21,3 +25,41 @@
 
 </html>
 
+
+<!--
+<?php
+$dbh = new PDO('mysql:host=localhost;dbname=fit2104_assignment2','fit2104','fit2104');
+$stmt = $dbh->prepare("SELECT * FROM `Client`");
+$stmt->execute();
+?>
+<table border="1">
+
+    <tr class ="rows">
+        <th>ID</th>
+        <th>First Name</th>
+        <th>Surname</th>
+        <th>Address</th>
+        <th>Phone</th>
+        <th>Email</th>
+        <th>Client Subscribed</th>
+        <th>Client Other Information</th>
+        <th>Actions</th>
+
+    </tr>
+    <?php while ($row = $stmt->fetchObject()):?>
+
+    <tr class = "rows">
+        <td><?php echo $row->Client_ID; ?> </td>
+        <td><?php echo $row->Client_FirstName; ?> </td>
+        <td><?php echo $row->Client_Surname; ?> </td>
+        <td><?php echo $row->Client_Address; ?> </td>
+        <td><?php echo $row->Client_Phone; ?> </td>
+        <td><?php echo $row->Client_Email; ?> </td>
+        <td><?php echo $row->Client_Subscribed; ?> </td>
+        <td><?php echo $row->Client_Other_Information; ?> </td>
+        <td>
+            <button onclick="window.location='updateclient.php?id=<?= $row->client_id ?>'">Update</button>
+            <button onclick="window.location='deleteclient.php?id=<?= $row->client_id ?>'">Delete</button>
+        </td>
+    <?php endwhile; ?>
+-->
