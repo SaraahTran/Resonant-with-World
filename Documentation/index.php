434112706f6de7a361ced5211131e850c3629083
Andy Li <alii0005@student.monash.edu>
2021-09-18 17:06:24 +1000

    Starting Documentation page with some information

--- Documentation/index.php
+++ Documentation/index.php
@@ -16,6 +16,76 @@
 
 <h1>Documentation</h1>
 
+<div class=" table-responsive">
+    <table class="table table-bordered responsive">
+        <thead>
+    <tr>
+        <th>Tasks</th>
+        <th>Martin Li 30584280</th>
+        <th>Sarah Tran</th>
+    </tr>
+    <tr>
+        <td>Clients</td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>Products</td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>Multiple Products</td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>Photoshoots</td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>Categories</td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>Images</td>
+        <td></td>
+        <td></td>
+    </tr>
+        </thead>
+    </table>
+</div>
+
+<h3>GitLab Repository</h3>
+<p> <a href="https://git.infotech.monash.edu/fit2104-cl/fit2104-2021-s2/pair_lab01_manzur_02/fit2104_assignment_2"><a href="https://git.infotech.monash.edu/fit2104-cl/fit2104-2021-s2/pair_lab01_manzur_02/fit2104_assignment_2">GitLab Repository</a></p>
+<br/>
+<h4>Credentials</h4>
+<p>Username: </p>
+<p>Password: </p>
+<br/>
+
+<h4>Database Scheme and Demo Data SQL</h4>
+<br/>
+
+<h4>Clients</h4>
+<br/>
+<h4>Products</h4>
+<br/>
+<h4>Photoshoots</h4>
+<br/>
+<h4>Categories</h4>
+<br/>
+
+
+
+<h4>MySQL</h4>
+<?php
+show_source("../connection.php");
+echo "<br/> ";
+?>
+
 
 </body>
 
