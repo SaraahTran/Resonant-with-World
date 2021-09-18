41e17ec175c3882b11c266aa5689cd4ca8246ffa
Sarah Tran <77770132+SaraahTran@users.noreply.github.com>
2021-09-18 23:48:46 +1000

    Delete selected categories

--- Categories/index.php
+++ Categories/index.php
@@ -10,63 +10,73 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;700;800&display=swap" rel="stylesheet">
 </head>
-
-<body>
-<?php include('../Menu/menu.php'); ?>
 <?php
-include('../connection.php');
-$dsn = "mysql:host=$db_host;dbname=$db_name";
-$dbh = new PDO($dsn, $db_username, $db_passwd);
-$stmt = $dbh->prepare("SELECT * FROM `Category`");
-$stmt->execute();
-?>
-<div class="container">
-    <h1>Categories</h1>
+include('../Menu/menu.php');
+include("../connection.php");
+/** @var PDO $dbh */
+//Now we'll process the POST request
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['Category_ID'])) {
+    //Noticed that we're adding questions marks (parameters) to the query
+    //To match number of selected items in POST request
+    $query_placeholders = trim(str_repeat("?,", count($_POST['Category_ID'])), ",");
+    $query = "DELETE FROM `Category` WHERE `Category_ID` in (" . $query_placeholders . ")";
+    $stmt = $dbh->prepare($query);
+    if ($stmt->execute($_POST['Category_ID'])) {
+        echo "<p class='message'>Selected category have been deleted</p>";
+    } else {
+        echo "<p class='message'>Error occurred while deleting titles</p>";
+    }
+} else {
+    echo "<p class='message'>Please select at least one title to delete: </p>";
+}
 
+$title_stmt = $dbh->prepare("SELECT * FROM `Category`");
+if ($title_stmt->execute() && $title_stmt->rowCount() > 0) { ?>
+        <div class="container">
+            <h1>Categories</h1>
     <button class="add-button" onclick="window.location='./Categories/insertCategories.php'">Add New Category</button>
-
-    <div class="table-responsive">
-        <table class="table table-bordered responsive">
-            <thead>
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Category Name</th>
-                <th scope="col">Actions</th>
-
+    <form method="post">
+        <input class="delete-selected-button" type="submit" value="Delete selected titles"/>
+        <div class="table-responsive">
+            <table class="table table-bordered responsive">
+                <thead>
+                <tr>
+                <th>Delete</th>
+                <th>ID</th>
+                <th>Name</th>
+                    <th>Action</th>
             </tr>
-            </thead>
-            <?php while ($row = $stmt->fetchObject()): ?>
-            <tbody>
-            <tr>
-                <td><?php echo $row->Category_ID; ?> </td>
-                <td><?php echo $row->Category_Name; ?> </td>
-                <td>
-                    <button class="action-button"
-                            onclick="window.location='./Categories/viewCategories.php?id=<?= $row->Category_ID ?>'">View
-                    </button>
-                    <button class="action-button"
-                            onclick="window.location='./Categories/updateCategories.php?id=<?= $row->Category_ID ?>'">
-                        Update
-                    </button>
-                    <button class="action-button"
-                            onclick="window.location='./Categories/deleteCategories.php?id=<?= $row->Category_ID ?>'">
-                        Delete
-                    </button>
-                </td>
-                <?php endwhile; ?>
-            </tr>
-            </tbody>
+                </thead>
+            <?php while ($row = $title_stmt->fetchObject()) { ?>
+                <tbody>
+                <tr>
+
+                    <td class="col-checkbox">
+                        <input type="checkbox" name="Category_ID[]" value="<?php echo $row->Category_ID; ?>"/>
+                    </td>
+                    <td><?= $row->Category_ID ?></td>
+                    <td><?= $row->Category_Name ?></td>
+<td>      <button class="action-button"
+                  onclick="window.location='./Categories/viewCategories.php?id=<?= $row->Category_ID ?>//'">View
+    </button>
+    <button class="action-button"
+            onclick="window.location='./Categories/updateCategories.php?id=<?//= $row->Category_ID ?>//'">
+        Update
+    </button>
+    <button class="action-button"
+            onclick="window.location='./Categories/deleteCategories.php?id=<?//= $row->Category_ID ?>//'">
+                         Delete
+                       </button></td>
+                </tr>
+            <?php } ?>
         </table>
+    </form>
+<?php } ?>
 
-
-    </div>
-
-</div>
-
-
-<?php include('../Menu/footer.php'); ?>
-
+            <?php include('../Menu/footer.php'); ?>    </div>
 </body>
+
+
 
 
 </html>
--- connection.php
+++ connection.php
@@ -3,4 +3,6 @@ $db_host = "localhost";
 $db_username = "fit2104";
 $db_passwd = "fit2104";
 $db_name = "fit2104_assignment2";
+$dsn = "mysql:host=$db_host;dbname=$db_name";
+$dbh = new PDO($dsn, $db_username, $db_passwd);
 ?>
