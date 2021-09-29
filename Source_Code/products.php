7648f57f80565d341bbb3f3c567061ddd387ab99
Andy Li <alii0005@student.monash.edu>
2021-09-29 20:41:19 +1000

    source code for multiple product

--- Source_Code/products.php
+++ Source_Code/products.php
@@ -11,4 +11,10 @@ show_source("../Products/deleteProducts.php");
 echo "<h1>INSERT PRODUCTS</h1>";
 echo "<br/> ";
 show_source("../Products/insertProducts.php");
+echo "<h1>INDEX MULTIPLE PRODUCTS</h1>";
+echo "<br/> ";
+show_source("../Products/editMultipleProductIndex.php");
+echo "<h1>EDIT MULTIPLE PRODUCTS</h1>";
+echo "<br/> ";
+show_source("../Products/editMultipleProducts.php");
 ?>
